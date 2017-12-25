1e8bcc280ef5bf93cbcad2abcf1207287e474014
Brandon Tran <brandon@Brandons-MacBook-Air.local>
2017-12-25 14:51:55 +0800

    promise version done

--- app-promise.js
+++ app-promise.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -12,3 +13,31 @@ const argv = yargs
   .help()
   .alias('help', 'h')
   .argv;
+
+  var encodeAddress = encodeURIComponent(argv.address);
+  var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}&key=AIzaSyAoxr4KcB9S2CEo_vQilG2Xx5kDlubOzH4`;
+
+  axios.get(geocodeURL).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+      throw new Error('Address no exist.')
+    }
+
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherURL = `https://api.darksky.net/forecast/d39372c23999bdfe6bca89a6cefac493/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address)
+
+    return axios.get(weatherURL);
+
+  }).then((resp) => {
+    var temperature = resp.data.currently.temperature;
+    var apparentTemperature = resp.data.currently.apparentTemperature;
+    console.log(`Temperature is ${temperature} but feels like ${apparentTemperature}.`);
+
+  }).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+      console.log("Can't connect to API.");
+    } else {
+      console.log(e.message);
+    }
+  });
